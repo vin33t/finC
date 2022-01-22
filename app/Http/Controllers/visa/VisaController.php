9f9577fa3c9f434473e93c749f5835af1d97acf0
vin33t <vineet.chd09@gmail.com>
2022-01-22 15:19:29 +0530

    FIxed Country with no visa error

--- app/Http/Controllers/visa/VisaController.php
+++ app/Http/Controllers/visa/VisaController.php
@@ -16,6 +16,8 @@ class VisaController extends Controller
         $countryPair = CountryPair::where('visaCountryParentId',$request->visaFrom)->where('visaCountryChildId',$request->visaTo)->get();
         if($countryPair->count()){
             $countryPair = $countryPair->first();
+        } else {
+            return redirect()->back();
         }
         return view('visa.visa')->with([
             'visaFrom' => $visaFrom,
